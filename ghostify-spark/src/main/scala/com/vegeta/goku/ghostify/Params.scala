0a27f13d4e74ea626a52f6d85447786f73e4b362
hdlldh <hdlldh@gmail.com>
2023-03-12 17:47:33 -0700

    Update config

--- ghostify-spark/src/main/scala/com/vegeta/goku/ghostify/Params.scala
+++ ghostify-spark/src/main/scala/com/vegeta/goku/ghostify/Params.scala
@@ -4,6 +4,6 @@ object Params {
 
   final val InputCol = "text"
   final val EmailRegex = "[\\w-\\._]+@([\\w-]+\\.)+[\\w-]{2,4}".r
-  final val ModelPath = "/Users/donglin.hu/Workspace/ghostify/utils/models/dslim/bert-base-NER/saved_model/1"
+  final val ModelPath = "/Users/donglin/Workspace/ghostify/utils/models/dslim/bert-base-NER/saved_model/1"
 //  final val ModelPath = "s3a://mce-datapipeline-amusing-rattler-usw2-qa-common/artifacts/ghostify/bert-base-NER/saved_model/1"
 }
